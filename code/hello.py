598612485e62b00ccc1b0c06c779a347df96d08d
Priti Desai <pdesai@us.ibm.com>
2018-08-08 11:13:45 -0700

    adding scripts/manifest

---
+++ code/hello.py
@@ -0,0 +1,8 @@
+def main(dict):
+    if 'name' in dict:
+        name = dict['name']
+    else:
+        name = "stranger"
+    greeting = "Hello " + name + "!"
+    print(greeting)
+    return {"greeting": greeting}
