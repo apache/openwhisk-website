068f6aab3b5b1395bf546cda60b45bac00fd61a6
jenkins <users@infra.apache.org>
2018-08-15 00:58:10 +0000

    Automatic Site Publish by Jenkins

--- code/hello.py
+++
@@ -1,8 +0,0 @@
-def main(dict):
-    if 'name' in dict:
-        name = dict['name']
-    else:
-        name = "stranger"
-    greeting = "Hello " + name + "!"
-    print(greeting)
-    return {"greeting": greeting}
