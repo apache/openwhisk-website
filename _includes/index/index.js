6431e09d6581e7b901c850625e238a0a0fe724d4
Priti Desai <pdesai@us.ibm.com>
2018-09-05 14:06:38 -0700

    Revert "move arrow to the anchor and nest the lists (#327)"
    This reverts commit 7628f735ba77a43da0d7b44bb92f472a2ffc04a2.

--- _includes/index/index.js
+++ _includes/index/index.js
@@ -1,29 +1,52 @@
-function applyCollapsible() {
-  var coll = document.getElementsByClassName("collapsible-toggle");
-  var i;
 
-  for (i = 0; i < coll.length; i++) {
-    // Set "click" event listener on all menuitems
-    coll[i].querySelector("a").addEventListener("click", function() {
-      var parent = this.parentElement;
-      var sibling = this.nextElementSibling;
-      // alternate menu open/close states
-      if (sibling !== null) {
-        if (parent.classList.contains("collapsible-toggle-toggled")) {
-          parent.classList.remove("collapsible-toggle-toggled");
-        } else {
-          parent.classList.add("collapsible-toggle-toggled");
-        }
+function applyCollapsible() {
+
+    var coll = document.getElementsByClassName("collapsible-toggle");
+    var i;
+
+    // Apply exapandable (arrows) to all menuitems with a sibling
+    for (i = 0; i < coll.length; i++) {
+      var sibling = coll[i].nextElementSibling;
+
+      if(sibling!==null){
+          coll[i].style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
+          // (top right bottom left)
+          sibling.style.padding = "0px 0px 0px 20px";
       }
-    });
-  }
+
+      // Set "click" event listener on all menuitems
+      coll[i].addEventListener("click", function() {
+        var sibling = this.nextElementSibling;
+        // alternate menu open/close states
+        if(sibling!==null){
+          if (sibling.style.display === "block") {
+            this.style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
+            sibling.style.display = "none";
+          } else {
+            this.style.listStyleImage = "url('../images/elements/arrow-down-12px.png')";
+            sibling.style.display = "block";
+          }
+        }
+      });
+    }
 }
 
+// If sizing of arrows becomes an issue, we can use background approach instead
+// li
+// {
+//     background: url(../images/arrow_icon.gif) no-repeat 4px 4px transparent;
+//     background-image: url("x.png");
+//     list-style-type: none;
+//     margin: 0;
+//     padding: 2px;
+//     vertical-align: middle;
+// }
+
 function openIndex() {
-  console.log("openIndex");
+          console.log("openIndex")
   document.getElementById("index").style.display = "block";
 }
 function closeIndex() {
-  console.log("closeIndex");
+            console.log("closeIndex")
   document.getElementById("index").style.display = "none";
 }
