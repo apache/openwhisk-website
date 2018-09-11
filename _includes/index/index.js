7231b69c7b4582e6deac5878e046297f39a8087d
Matt Rutkowski <mrutkows@us.ibm.com>
2018-09-11 14:57:47 -0500

    Add support for collapsible sections

--- _includes/index/index.js
+++ _includes/index/index.js
@@ -1,42 +1,76 @@
 
 function applyCollapsible() {
+  addListenersToIndex();
+  addListenersToSections();
+}
 
-    var coll = document.getElementsByClassName("collapsible-toggle");
-    var i;
+function addListenersToIndex()
+{
+  var coll = document.getElementsByClassName("index-menu-toggle");
+  var i;
 
-    // Apply exapandable (arrows) to all menuitems with a sibling
-    for (i = 0; i < coll.length; i++) {
-      var sibling = coll[i].nextElementSibling;
+  // Apply exapandable (arrows) to all menuitems with a sibling
+  for (i = 0; i < coll.length; i++) {
+    var sibling = coll[i].nextElementSibling;
 
+    if(sibling!==null){
+        coll[i].style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
+        // (top right bottom left)
+        sibling.style.padding = "0px 0px 0px 20px";
+    }
+
+    // Set "click" event listener on all menuitems
+    coll[i].addEventListener("click", function() {
+      var sibling = this.nextElementSibling;
+      // toggle menu open/close states
       if(sibling!==null){
-          coll[i].style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
-          // (top right bottom left)
-          sibling.style.padding = "0px 0px 0px 20px";
+        if (sibling.style.display === "block") {
+          this.style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
+          sibling.style.display = "none";
+        } else {
+          this.style.listStyleImage = "url('../images/elements/arrow-down-12px.png')";
+          sibling.style.display = "block";
+        }
       }
 
-      // Set "click" event listener on all menuitems
-      coll[i].addEventListener("click", function() {
-        var sibling = this.nextElementSibling;
-        // toggle menu open/close states
-        if(sibling!==null){
-          if (sibling.style.display === "block") {
-            this.style.listStyleImage = "url('../images/elements/arrow-right-12px.png')";
-            sibling.style.display = "none";
-          } else {
-            this.style.listStyleImage = "url('../images/elements/arrow-down-12px.png')";
-            sibling.style.display = "block";
-          }
-        }
+    });
+  }
+}
 
-      });
+function addListenersToSections()
+{
+  var coll = document.getElementsByClassName("section-toggle");
+  var i;
+
+  // Apply exapandable (arrows) to all menuitems with a sibling
+  for (i = 0; i < coll.length; i++) {
+    var sibling = coll[i].nextElementSibling;
+
+    if(sibling!==null){
+      if(coll[i].classList.contains("section-toggle-start-open")){
+        coll[i].style.backgroundImage = "url('../images/elements/circle-minus.png')";
+      } else {
+        coll[i].style.backgroundImage = "url('../images/elements/circle-plus.png')";
+      }
     }
-}
 
-function openIndex() {
-          console.log("openIndex")
-  document.getElementById("index").style.display = "block";
-}
-function closeIndex() {
-            console.log("closeIndex")
-  document.getElementById("index").style.display = "none";
+    // Set "click" event listener on all menuitems
+    coll[i].addEventListener("click", function() {
+      var sibling = this.nextElementSibling;
+      // toggle menu open/close states
+      if(sibling!==null){
+        console.log("sibling.style.display: ["+sibling.style.display+"]");
+        if (sibling.style.display === "block") {
+          console.log("collapsing");
+          sibling.style.display = "none";
+          this.style.backgroundImage = "url('../images/elements/circle-plus.png')";
+        } else {
+          console.log("expanding");
+          sibling.style.display = "block";
+          this.style.backgroundImage = "url('../images/elements/circle-minus.png')";
+        }
+      }
+
+    });
+  }
 }
