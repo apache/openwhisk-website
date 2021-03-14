cd559c72b9872fe05fcb80a59c058bcb308c8644
John Bampton <jbampton@gmail.com>
2021-03-15 03:55:21 +1000

    chore: minor Ruby linting and fix end of file

--- _plugins/remote_file_content.rb
+++ _plugins/remote_file_content.rb
@@ -2,15 +2,13 @@ require 'net/http'
 require 'uri'
 
 module Jekyll
-
   class RemoteFileContent < Liquid::Tag
-
     def initialize(tag_name, markup, tokens)
       url = markup
 
-      puts 'Fetching content of url: ' + url
+      puts "Fetching content of url: #{url}"
 
-      if url =~ URI::regexp
+      if url =~ URI::DEFAULT_PARSER.make_regexp
         @content = fetchContent(url)
       else
         raise 'Invalid URL passed to RemoteFileContent'
@@ -19,12 +17,8 @@ module Jekyll
       super
     end
 
-    def render(context)
-      if @content
-        @content
-      else
-        raise 'Something went wrong in RemoteFileContent'
-      end
+    def render(_context)
+      @content || raise('Something went wrong in RemoteFileContent')
     end
 
     def fetchContent(url)
